42ad6c66894472b05b9945832b786137b424234f
RushikeshPadaki <rushikeshpadaki345@gmail.com>
2025-02-10 17:29:11 +0530

    Adding 5 assignment programs from If-Else Conditional Statements class.

--- 00_BEGINNER_COURSE/01_ASSIGNMENTS/02_IF_ELSE_CONDITIONAL_STATEMENTS/OddOrEven.java
+++ 00_BEGINNER_COURSE/01_ASSIGNMENTS/02_IF_ELSE_CONDITIONAL_STATEMENTS/OddOrEven.java
@@ -1,3 +1,19 @@
+/*
+Title: Odd or Even
+Problem Statement: Determine whether a given number is odd or even
+Author: Rushikesh Padaki
+Date: 10/02/2025
+
+Description:
+This program takes an integer input from the user and determines whether it is odd or even. It uses a separate method `isEven(int num)` to check divisibility by 2 using the modulus operator (%). If the number is divisible by 2, it is classified as even; otherwise, it is classified as odd.
+
+Sample Output(s):
+Enter a number: 8
+The entered number is Even!
+
+Enter a number: 15
+The entered number is Odd!
+*/
 import java.util.Scanner;
 
 public class OddOrEven {
